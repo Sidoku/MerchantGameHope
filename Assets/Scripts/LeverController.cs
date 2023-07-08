dc0159b7ceff786400d426ee86c968d187f19ac8
abhinav1925 <31373821+abhinav1925@users.noreply.github.com>
2023-07-08 23:47:41 +0530

    Fixed bridge

--- Assets/Scripts/LeverController.cs
+++ Assets/Scripts/LeverController.cs
@@ -7,19 +7,19 @@ public class LeverController : MonoBehaviour
 {
     public GameObject BridgeObject;
 
-    public HingeJoint2D bridgeJoint;
+    //public HingeJoint2D bridgeJoint;
 
     bool isBridgeOpen = false;
     void Start()
     {
-        bridgeJoint = BridgeObject.GetComponent<HingeJoint2D>();
-        bridgeJoint.useMotor = true;
+        //bridgeJoint = BridgeObject.GetComponent<HingeJoint2D>();
+       // bridgeJoint.useMotor = true;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
-        var motor = bridgeJoint.motor;
+        //var motor = bridgeJoint.motor;
 
         Debug.Log(collision.gameObject.name.ToString());
         if (collision.gameObject.name.ToString().Equals("PF Player"))
@@ -27,12 +27,12 @@ public class LeverController : MonoBehaviour
             Debug.Log("This works");
             if (isBridgeOpen)
             {
-                CloseBridge(motor);
+                BridgeObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
                 isBridgeOpen = false;
             }
             else
             {
-                OpenBridge(motor);
+                BridgeObject.transform.rotation = Quaternion.Euler(0f, 0f, -90f);
                 isBridgeOpen = true;
             }
         }
@@ -40,12 +40,12 @@ public class LeverController : MonoBehaviour
 
     private void OpenBridge(JointMotor2D motor)
     {
-        motor.motorSpeed = 20.0f;
+        //motor.motorSpeed = 20.0f;
     }
 
     private void CloseBridge(JointMotor2D motor) 
     { 
-        motor.motorSpeed = -20.0f;
+        //motor.motorSpeed = -20.0f;
     }
     
 }
